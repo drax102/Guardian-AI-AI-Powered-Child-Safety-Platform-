22947a6280bd7cf338e67055bc9b073af18be09a
drax102 <utkarshistheine@gmail.com>
2026-05-25 00:09:51 +0530

    fix production cors

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+
+    allow_origins=[
+        "http://localhost:5173",
+        "https://guardian-ai-ai-powered-child-safety.vercel.app"
+    ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"]
 )
 
